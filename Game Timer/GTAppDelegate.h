9c5a97c5f257e5a27d1165b3889a29ed6dfa1194
Yasser Al-Khder <yasser.alkhder@gmail.com>
2013-08-31 08:35:21 -0400

    Initial Commit

---
+++ Game Timer/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  Game Timer
+//
+//  Created by Yasser Al-Khder on 2013-08-31.
+//  Copyright (c) 2013 Yasser Al-Khder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Game Timer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Game Timer
+//
+//  Created by Yasser Al-Khder on 2013-08-31.
+//  Copyright (c) 2013 Yasser Al-Khder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GTAppDelegate class]));
+    }
+}
